6f0bd3dd1b155a9c402ef5afac0250bc306c4c49
CiVediamo <108425163+CiVediamo@users.noreply.github.com>
2023-09-05 11:08:49 +0300

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|=======================================================================");
+            Console.WriteLine("|");
+            Console.WriteLine("| Work #1. Complited a student Lysenko Artem");
+            Console.WriteLine("|");
+            Console.Write("|-----------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("|");
+            Console.WriteLine("| Group: 90002298");
+            Console.WriteLine("|");
+            Console.Write("|-----------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("|");
+            Console.WriteLine("| Birth date: 26th of March, 2006");
+            Console.WriteLine("|");
+            Console.Write("|-----------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("|");
+            Console.WriteLine("| From: Belgorod");
+            Console.WriteLine("|");
+            Console.Write("|-----------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("|");
+            Console.WriteLine("| Favorite subject: Math");
+            Console.WriteLine("|");
+            Console.Write("|-----------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("|");
+            Console.WriteLine("| Hobby: walk in the park, write computer programs, play with my cat");
+            Console.WriteLine("|");
+            Console.WriteLine("|=======================================================================");
+        }
+    }
+}
+
+
+//Console.WriteLine("Work #1. Complited a student Lysenko Artem");
+//Console.WriteLine("Group: 90002298");
+//Console.WriteLine("Birth date: 26th of March, 2006");
+//Console.WriteLine("From: Belgorod");
+//Console.WriteLine("Favourite subject: Math");
+//Console.WriteLine("Hobby: walk in the park, write computer programs, play with my cat");
